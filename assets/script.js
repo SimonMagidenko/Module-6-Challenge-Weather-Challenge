5bba3f35110159501f578392e11c35f7bb96925d
SimonM <smagidenko@gmail.com>
2023-08-07 21:19:15 -0400

    edited formatting

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 
-/// put this fetch request inside event listener for the search button
 let apiKey = "0aac185f72bc2b9af05c0fe396e10820"
 
 fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`)
@@ -9,13 +8,15 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${api
         console.log(firstCity.lat);
         console.log(firstCity.lon);
 
-        // return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${apiKey}`)
+
 
     })
 
-    // .then(response => response.json())
-    // .then(data => {
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${apiKey}`)
 
-    //     console.log(data);
-    // })
+    .then(response => response.json())
+    .then(data => {
+
+        console.log(data);
+    })
 
