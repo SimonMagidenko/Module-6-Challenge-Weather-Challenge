1665491ee2802bd84fc7844799c3c0e826c72171
SimonM <smagidenko@gmail.com>
2023-08-07 23:08:01 -0400

    added API hooks with parameters. adding dynamic html to populate onto page.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,42 @@
 
 let apiKey = "0aac185f72bc2b9af05c0fe396e10820"
 
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(citiesFound => {
-        let firstCity = citiesFound[0];
-        console.log(firstCity.lat);
-        console.log(firstCity.lon);
+return fetchCity()
+
+function fetchCity() {
+    let citySearched = document.querySelector("#searchBarInput").value
+    console.log(citySearched)
+    let cityName = document.querySelector("#cityName")
+    cityName.textContent = citySearched
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${citySearched}&limit=5&appid=${apiKey}`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (citiesFound) {
+            console.log(citiesFound)
+            let firstCity = citiesFound[0];
+            console.log(firstCity.lon)
+            console.log(firstCity.lat)
 
 
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${apiKey}`)
 
-    })
+        })
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
 
-    .then(response => response.json())
-    .then(data => {
+            console.log(data);
+        })
+}
 
-        console.log(data);
-    })
+function searchCity() {
+    let citySearched = document.querySelector("#searchBarInput").value
+    let cityName = document.querySelector("#cityName")
+    cityName.textContent = citySearched
+}
+
+let searchButton = document.querySelector("#searchButton");
+searchButton.addEventListener("click", searchCity);
 
