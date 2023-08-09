2ebf14b88a7e77ce9806be3c2b2b573d06daf63a
SimonM <smagidenko@gmail.com>
2023-08-09 10:04:39 -0400

    added functionality to Javascript, for loops

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ let apiKey = "0aac185f72bc2b9af05c0fe396e10820"
 
 function fetchCity(city) {
     console.log(city)
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${apiKey}`)
         .then(function (response) {
             return response.json()
         })
@@ -16,16 +16,37 @@ function fetchCity(city) {
             console.log(firstCity.lat)
 
 
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${apiKey}`)
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&units=imperial&appid=${apiKey}`)
 
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (forecast) {
+                    console.log(forecast)
+
+                    let forecastCard = document.querySelectorAll(".card-body")
+                    console.log(forecastCard)
+                    let number = 0
+                    for (let i = 0; i < forecastCard.length; i++) {
+                        let forecastCardElement = forecastCard[i];
+                        let forecastTemp = forecastCardElement.querySelector(".forecastTemp")
+                        console.log(forecastTemp)
+                        forecastTemp.textContent = forecast.list[number].main.temp
+
+                        //insert icon here
+                        //input forecast Wind here
+                        let forecastWind = forecastCardElement.querySelector(".forecastWind")
+                        forecastWind.textContent = forecast.list[number].wind.speed
+                        //input forecast Humidity here
+                        let forecastHumidity = forecastCardElement.querySelector(".forecastHumidity")
+                        forecastHumidity.textContent = forecast.list[number].main.humidity
+                        number += 7
+
+
+                    }
+                })
         })
 
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (forecast) {
-            console.log(forecast)
-        })
 }
 
 function searchCity() {
@@ -33,7 +54,9 @@ function searchCity() {
     let cityName = document.querySelector("#cityName");
     cityName.textContent = citySearched;
     console.log(citySearched)
-    fetchCity(citySearched);
+    let cityTemp = document.querySelector("#cityTemp")
+    cityTemp.textContent =
+        fetchCity(citySearched);
 }
 
 let searchButton = document.querySelector(".searchButton");
