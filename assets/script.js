17c0f7bb493b0af549852134c96f4679094bef1b
SimonM <smagidenko@gmail.com>
2023-08-09 15:05:25 -0400

    made JS changes

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 
 let apiKey = "0aac185f72bc2b9af05c0fe396e10820"
 
+
+
 function fetchCity(city) {
     console.log(city)
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${apiKey}`)
@@ -23,21 +25,22 @@ function fetchCity(city) {
                 })
                 .then(function (forecast) {
                     console.log(forecast)
-
+                    //TODO input todays information here and reset number to 0 
                     let forecastCard = document.querySelectorAll(".card-body")
                     console.log(forecastCard)
-                    let number = 0
+                    let number = 7
                     for (let i = 0; i < forecastCard.length; i++) {
+                        //TODO input forecast Temp here
                         let forecastCardElement = forecastCard[i];
                         let forecastTemp = forecastCardElement.querySelector(".forecastTemp")
-                        console.log(forecastTemp)
-                        forecastTemp.textContent = forecast.list[number].main.temp
+                        forecastTemp.textContent = "Temperature: " + forecast.list[number].main.temp + " Degress"
 
                         //insert icon here
-                        //input forecast Wind here
+
+                        //TODO input forecast Wind here
                         let forecastWind = forecastCardElement.querySelector(".forecastWind")
                         forecastWind.textContent = forecast.list[number].wind.speed
-                        //input forecast Humidity here
+                        //TODO input forecast Humidity here
                         let forecastHumidity = forecastCardElement.querySelector(".forecastHumidity")
                         forecastHumidity.textContent = forecast.list[number].main.humidity
                         number += 7
@@ -51,12 +54,10 @@ function fetchCity(city) {
 
 function searchCity() {
     let citySearched = document.querySelector("#searchBarInput").value;
-    let cityName = document.querySelector("#cityName");
-    cityName.textContent = citySearched;
+    let cityNameEl = document.querySelector("#cityName");
+    cityNameEl.textContent = citySearched;
     console.log(citySearched)
-    let cityTemp = document.querySelector("#cityTemp")
-    cityTemp.textContent =
-        fetchCity(citySearched);
+    fetchCity(citySearched);
 }
 
 let searchButton = document.querySelector(".searchButton");
