46039f669ea7fcccc1daa82f65d56190e7fd361e
SimonM <smagidenko@gmail.com>
2023-09-06 19:41:52 -0400

    added functionality on card rendering

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,15 @@
-
 let apiKey = "0aac185f72bc2b9af05c0fe396e10820"
 
-
-
 function fetchCity(city) {
-    console.log(city)
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${apiKey}`)
         .then(function (response) {
             return response.json()
         })
         .then(function (citiesFound) {
-            console.log(citiesFound)
             let firstCity = citiesFound[0];
             let firstCityLon = citiesFound[0].lon
             let firstCityLat = citiesFound[0].lat
-            console.log(firstCity.lon)
-            console.log(firstCity.lat)
+
 
 
             fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&units=imperial&appid=${apiKey}`)
@@ -25,28 +19,42 @@ function fetchCity(city) {
                 })
                 .then(function (forecast) {
                     console.log(forecast)
-                    //TODO input todays information here and reset number to 0 
+                    // Sets text of current temperature 
+                    cityTempNow = document.querySelector("#cityTempNow")
+                    cityTempNow.textContent = forecast.list[0].main.temp + " °F"
+                    // Sets text of current wind
+                    cityWindNow = document.querySelector("#cityWindNow")
+                    cityWindNow.textContent = forecast.list[0].wind.speed + " Mph"
+                    // Sets text of current humidity
+                    cityHumidityNow = document.querySelector("#cityHumidityNow")
+                    cityHumidityNow.textContent = forecast.list[0].main.humidity + " %"
+
                     let forecastCard = document.querySelectorAll(".card-body")
                     console.log(forecastCard)
                     let number = 7
                     for (let i = 0; i < forecastCard.length; i++) {
-                        //TODO input forecast Temp here
                         let forecastCardElement = forecastCard[i];
+                        // Forecast Icon
+                        let forecastIcon = forecastCardElement.querySelector(".forecastIcon")
+                        let getOpenWeatherIcon = forecast.list[number].weather[0].id
+                        forecastIcon.textContent = `http://openweathermap.org/img/w/${getOpenWeatherIcon}.png
+                        `
+                        // Forecast Temp 
                         let forecastTemp = forecastCardElement.querySelector(".forecastTemp")
-                        forecastTemp.textContent = "Temperature: " + forecast.list[number].main.temp + " Degress"
+                        forecastTemp.textContent = "Temp: " + forecast.list[number].main.temp + " °F"
 
-                        //insert icon here
-
-                        //TODO input forecast Wind here
+                        // Forecast Wind 
                         let forecastWind = forecastCardElement.querySelector(".forecastWind")
-                        forecastWind.textContent = forecast.list[number].wind.speed
-                        //TODO input forecast Humidity here
+                        forecastWind.textContent = "Wind " + forecast.list[number].wind.speed + " mph"
+
+                        // Forecast Humidity 
                         let forecastHumidity = forecastCardElement.querySelector(".forecastHumidity")
-                        forecastHumidity.textContent = forecast.list[number].main.humidity
+                        forecastHumidity.textContent = "Humidity: " + forecast.list[number].main.humidity + " %"
+
                         number += 7
-
-
                     }
+
+
                 })
         })
 
@@ -54,9 +62,8 @@ function fetchCity(city) {
 
 function searchCity() {
     let citySearched = document.querySelector("#searchBarInput").value;
-    let cityNameEl = document.querySelector("#cityName");
+    let cityNameEl = document.querySelector("#cityNameNow");
     cityNameEl.textContent = citySearched;
-    console.log(citySearched)
     fetchCity(citySearched);
 }
 
